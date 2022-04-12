15d51de5b48249ea4a4980b3a89082080529e0a9
Nasroallah Elidrissi <nelidris@e2r3p2.1337.ma>
2022-04-12 05:19:16 +0000

    SETUP

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ classifiers = [
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet',
         'Topic :: Utilities',
         'Topic :: Software Development :: Libraries',
