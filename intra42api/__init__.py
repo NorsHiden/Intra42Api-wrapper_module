4f4a298efad89c2aaba41ef51703e414c0b466c7
Nasroallah Elidrissi <nelidris@e2r3p2.1337.ma>
2022-04-12 04:51:30 +0000

    modules

---
+++ intra42api/__init__.py
@@ -0,0 +1,9 @@
+"""A simple wrapper for the 42Network API"""
+
+__title__ = 'intra42network-wrapper'
+__author__ = 'Nors'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019-2022 Nors'
+__version__ = '1.0.0'
+
+from .intra42 import Intra42
